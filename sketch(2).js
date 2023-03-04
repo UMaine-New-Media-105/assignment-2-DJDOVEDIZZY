4a483b3a9a3375852ab4a40e811f71baaaa24b6e
DJDOVEDIZZY <cyprian.dove@maine.edu>
2023-03-03 21:06:19 -0500

    CYPRIAN DOVE- NMD 02-03.js
    https://editor.p5js.org/cyprian.dove/sketches/AWQBFbXKF
    Fit and centered onto the canvas

---
+++ sketch(2).js
@@ -0,0 +1,27 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(color("darkgreen"));
+  noStroke();
+  scale(2.6);
+  translate(0,60);
+  addShape();
+}
+function addShape(x,y){
+  push();
+  beginShape();
+vertex(5, 8);
+vertex(30, 5);
+vertex(30, 20);
+vertex(85, -5);
+    vertex(150, 5);
+vertex(85, 60);
+  vertex(125, 60);
+    vertex(85, 40);
+vertex(105, 60);
+  vertex(50, 60);
+endShape(CLOSE);
+  pop();
+}
